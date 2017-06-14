d5767a9c607557ce248b30af9393591ee52ff075
harrieschen <harrieschen@gmail.com>
2017-06-14 17:48:49 +0200

    fix typo

--- app/controllers/spree/api/alipay_controller.rb
+++ app/controllers/spree/api/alipay_controller.rb
@@ -3,88 +3,64 @@ module Spree
     module V1
       class AlipayController < Spree::Api::BaseController
 
-        def index
-
-        end
-
-
-        {"notify_id"=>"87febfa3a71272e6541e81411efcfb6lbq",
-         "notify_type"=>"trade_status_sync",
-         "sign"=>"Ti72p03vv56razlmtOwNF/8QjTTdbYytMHGgKRZMkIWcEKf8bjPuMpWcpZcHRT8mHSt4pBsqc9pm9q4VJCiepthuU1fcEHQL9XJnZnVrPMauisFm4BttteAO0F14w1wXvRlCyNeJdmeMtfYT24c8wPf6XPv9BCLabQc/QEu1ySk=",
-         "trade_no"=>"2017061421001003690222511005",
-         "total_fee"=>"0.02",
-         "out_trade_no"=>"Q83TGF2G5PQ1CLT",
-         "currency"=>"EUR",
-         "notify_time"=>"2017-06-14 01:42:16",
-         "trade_status"=>"TRADE_FINISHED",
-         "sign_type"=>"RSA"}
         def notify
-
+          notify_type = params['notify_type']
+          return notify_error unless notify_type == 'trade_status_sync'
+          trade_status = params['trade_status']
+          return notify_error unless trade_status == 'TRADE_FINISHED'
 
           out_trade_no = params['out_trade_no']
-
           trade_nos = out_trade_no.split('-')
-          paymenr_number = trade_nos[1]
-
-          payment = Spree::Payment.find_by_number(paymenr_number)
-
+          payment_number = trade_nos[1]
+          payment = Spree::Payment.find_by_number(payment_number)
           payment.response_code = params[:trade_no]
           payment.state = 'completed'
           payment.save
-
           response = ActiveMerchant::Billing::Response.new(
-              true,
-              'Transaction successful',
-              params
+            true,
+            'Transaction successful',
+            params
+          )
+          payment.log_entries.create(
+            details: response.to_yaml
           )
 
-          payment.log_entries.create(
-              :details => response.to_yaml
-          )
+          @order = find_order(trade_nos[0])
+          @order.next unless @order.completed?
 
           render json: {
-              'result' => true
+            success: true
           }
         end
 
-
-        MOBILE_SECURITY_PARAMS = %w( trade_no out_trade_no )
         def mobile_security
-# check_required_params(params, MOBILE_SECURITY_PARAMS)
-
-# notify_url out_trade_no subject total_fee body
-
-          @order = find_order
-
+          @order = find_order(params[:order_number])
           payment_params = {
-              amount: @order.total,
-              payment_method: payment_method
+            amount: @order.total,
+            payment_method: payment_method
           }
           payment = @order.payments.create!(payment_params)
-
           out_trade_no = @order.number + '-' + payment.number
-
           url = payment_method.set_mobile_security_url(out_trade_no,@order, {})
-
           render json: {
-              "url" => url
+            url: url
           }
         end
 
-
         private
-        def self.check_required_params(params, names)
-          names.each do |name|
-            warn("Alipay Warn: missing required option: #{name}") unless params.has_key?(name)
-          end
-        end
 
         def payment_method
           Spree::PaymentMethod.find_by_type('Spree::Gateway::AlipayTrade') || raise(ActiveRecord::RecordNotFound)
         end
 
-        def find_order
-          @order = Spree::Order.find_by_number!(params[:order_number]) || raise(ActiveRecord::RecordNotFound)
+        def find_order(order_number)
+          @order = Spree::Order.find_by_number!(order_number) || raise(ActiveRecord::RecordNotFound)
+        end
+
+        def notify_error
+          render json: {
+            success: false
+          }
         end
 
       end
--- app/models/spree/gateway/alipay_trade.rb
+++ app/models/spree/gateway/alipay_trade.rb
@@ -59,14 +59,14 @@ module Spree
       subject      = gateway_options[:subject] || order.number
       total_amount = order.total
       options = {
-          :out_trade_no      => out_trade_no,
-          :subject           => subject,
-          :currency          => 'EUR',
-          :total_fee         => 0.01,
-          :return_url        => preferred_return_url,
-          :notify_url        => preferred_notify_url,
-          :body              => 'sssssss',
-          :forex_biz          => 'FP'
+        :out_trade_no      => out_trade_no,
+        :subject           => subject,
+        :currency          => 'EUR',
+        :total_fee         => total_amount,
+        :return_url        => preferred_return_url,
+        :notify_url        => preferred_notify_url,
+        :body              => 'sssssss',
+        :forex_biz          => 'FP'
       }
 
       provider.mobile_securitypay_pay_string(options)
@@ -76,45 +76,26 @@ module Spree
       %w(credit)
     end
 
-    def set_forex_trade(out_trade_no, order, return_url, notify_url, gateway_options={})
-      raise unless preferred_pid && preferred_key && preferred_seller_email
-
-      subject      = gateway_options[:subject] || order.number
-
-      options = {
-          :out_trade_no      => out_trade_no,
-          :subject           => subject,
-          :currency          => "EUR",
-          :total_fee         => total_amount,
-          :return_url        => return_url,
-          :notify_url        => notify_url
-      }
-
-      provider.create_forex_trade_url(options)
-    end
-
     def refund(money, source, gateway_options)
       payment = Spree::Payment.find_by(:response_code => source)
       out_return_no = source + Time.now.strftime('%Y%m%d%H%M%S')
       out_trade_no = payment.order.number + '-' + payment.number
+
       params = {
-          :out_return_no => out_return_no,
-          :out_trade_no => out_trade_no,
-          :currency => 'EUR',
-          :return_amount => 0.01,
-          :reason => gateway_options[:originator].reason.name,
+        :out_return_no => out_return_no,
+        :out_trade_no => out_trade_no,
+        :currency => 'EUR',
+        :return_amount => money / 100.0.to_d,
+        :reason => gateway_options[:originator].reason.name
       }
       url = provider.forex_refund_url(params, {:key => preferred_MD5_key, :sign_type => 'MD5'})
       xml_result = Net::HTTP.get(url)
       hash_result  = Hash.from_xml(xml_result)
-      if hash_result["alipay"]["is_success"] == 'T'
+      if hash_result['alipay']['is_success'] == 'T'
         ActiveMerchant::Billing::Response.new(true, 'Refund success', {}, :test => false, :authorization => out_return_no)
       else
         ActiveMerchant::Billing::Response.new(false, 'Refund Failed:' + hash_result['alipay']['error'], {}, :test => false)
       end
-
-
-
     end
 
     private
